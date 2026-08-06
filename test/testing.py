16550dc537d1ba912185d5d8ce63b7c21c2c133a
feobqru <theofkbarnes@gmail.com>
2026-08-06 14:02:42 +1200

    Graph now plots based on the initial price invested

--- test/testing.py
+++ test/testing.py
@@ -1,20 +1,30 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-
+from datetime import datetime
 
 start_date = input("When did you buy your phone? (YYYY-MM-DD): ")
-stock_data = yf.download("AAPL", start=start_date, end="2026-01-01")
-stock_data.head() 
+end_date = datetime.today().strftime('%Y-%m-%d')
 
+purchase_price = float(input("What was the price of your phone? (in USD): "))
+
+stock_data = yf.download("AAPL", start=start_date, end=end_date)
+stock_data.head() 
+prices=stock_data['Close']
+
+initial_price = prices.iloc[0]
+shares_owned = purchase_price / initial_price
+stock_data['predicted_value'] = prices * shares_owned
+
+print(f"Shares owned: {shares_owned}")
 
 plt.figure(figsize=(10, 6))
-plt.plot(stock_data['Close'], label='Closing Price', color='blue')
-plt.title("Apple Stock Closing Prices (2022)")
+plt.plot(stock_data.index, stock_data['predicted_value'], color='blue')
+plt.title("Apple Stock Closing Prices")
+plt.axhline(purchase_price, color='red', linestyle=':', label=f'Initial ${purchase_price:.2f} Principal')
 plt.xlabel("Date")
 plt.ylabel("Price (USD)")
 plt.legend()
 plt.grid()
 plt.show()
 
-start_date = input("When did you buy your phone? (YYYY-MM-DD): ")
 
