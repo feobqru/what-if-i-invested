4484fb1722546bda76bbc292960432ca45064955
feobqru <theofkbarnes@gmail.com>
2026-08-24 11:31:17 +1200

    IT FINALLY WOKRS - Accepts information from form to then plot the graph

--- stock_pull.py
+++ stock_pull.py
@@ -1,23 +1,46 @@
 import yfinance as yf
 from datetime import datetime
 import json
-import os
+from flask import Flask, render_template, request, jsonify
 
-start_date = input("When did you buy your phone? (YYYY-MM-DD): ")
-end_date = datetime.today().strftime('%Y-%m-%d')
-purchase_price = float(input("What was the price of your phone? (in USD): "))
+app = Flask(__name__)
 
-stock_data = yf.download("AAPL", start=start_date, end=end_date)
-prices=stock_data['Close']
-initial_price = prices.iloc[0]
-shares_owned = purchase_price / initial_price
-stock_data['predicted_value'] = prices * shares_owned
+@app.route('/get_chart_data')
+def get_chart_data():
+    with open("chart_data.json", "r") as f:
+        data = json.load(f)
+    return jsonify(data)
 
-data = {
-    "dates": stock_data.index.strftime('%Y-%m-%d').tolist(),
-    "values": stock_data['predicted_value'].tolist(),
-    "purchase_price": purchase_price
-}
 
-with open("chart_data.json", "w") as f:
-    json.dump(data, f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/submit-form', methods=["POST"])
+def handle_form():
+    date_received = request.form.get("date")
+    phone_received = float(request.form.get("phones"))
+    start_date = date_received
+    end_date = datetime.today().strftime('%Y-%m-%d')
+    purchase_price = phone_received
+
+    stock_data = yf.download("AAPL", start=start_date, end=end_date)
+    prices=stock_data['Close']
+    initial_price = prices.iloc[0]
+    shares_owned = purchase_price / initial_price
+    stock_data['predicted_value'] = prices * shares_owned
+
+    data = {
+        "dates": stock_data.index.strftime('%Y-%m-%d').tolist(),
+        "values": stock_data['predicted_value'].tolist(),
+        "purchase_price": purchase_price
+    }
+
+    with open("chart_data.json", "w") as f:
+        json.dump(data, f)
+    return render_template("graph_page.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
